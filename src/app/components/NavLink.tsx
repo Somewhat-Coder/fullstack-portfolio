80a8d31967a9d5f7d96e204f0d93b63b17b6f422
Sammam Sohail <65643349+Somewhat-Coder@users.noreply.github.com>
2025-05-07 02:31:33 +0100

    improved navbar

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -5,7 +5,7 @@ const NavLink = ({ href, title }: { href: string; title: string }) => {
 return(
     <Link
     href={href}
-    className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+    className="block py-2 pl-3 pr-4 text-[#f5f5f5] sm:text-xl rounded md:p-0 hover:text-purple-400"
   >
     {title}
   </Link>
